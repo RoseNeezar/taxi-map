701b93a664a8d29f17ee797029a5b6a621d3bbc8
Neezar <rforlyffe@gmaicl.com>
2021-07-19 01:03:17 +0800

    add error page

---
+++ src/app/pageComponent/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Head from "next/head";
+import Navigate from "../../utils/Navigate";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Welp...</title>
+      </Head>
+      <div className="h-screen w-full flex justify-center items-center bg-gray-800 flex-col">
+        <h1 className="text-3xl text-white mb-10">Location not enable ... </h1>
+        <button
+          onClick={() => Navigate?.push("/")}
+          className="p-4 rounded-xl bg-gray-500 text-white text-3xl"
+        >
+          Go Back
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default ErrorPage;
