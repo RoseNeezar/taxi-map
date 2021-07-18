3bb43a72ff27b9203b1763e6b86d8ad9489c6ea5
Neezar <rforlyffe@gmaicl.com>
2021-07-18 23:06:01 +0800

    update header

--- src/app/component/LoadingPage.tsx
+++ src/app/component/LoadingPage.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Head from "next/head";
 
 const LoadingPage = () => {
   return (
-    <div className="h-screen w-full flex justify-center items-center bg-gray-800 flex-col">
-      <h1 className="text-3xl text-white mb-10">Fetching Location ...</h1>
-      <div className=" loading-spinner" />
-    </div>
+    <>
+      <Head>
+        <title>Loading...</title>
+      </Head>
+      <div className="h-screen w-full flex justify-center items-center bg-gray-800 flex-col">
+        <h1 className="text-3xl text-white mb-10">Fetching Location ...</h1>
+        <div className=" loading-spinner" />
+      </div>
+    </>
   );
 };
 
--- src/app/pageComponent/NotFound/NotFound.tsx
+++ src/app/pageComponent/NotFound/NotFound.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import Head from "next/head";
 
 const NotFound = () => {
   const history = useHistory();
   return (
-    <div className="h-screen w-full flex justify-center items-center bg-gray-800 flex-col">
-      <h1 className="text-3xl text-white mb-10">Not Found</h1>
-      <button
-        onClick={() => history.push("/")}
-        className="p-4 rounded-xl bg-gray-500 text-white text-3xl"
-      >
-        Go Back
-      </button>
-    </div>
+    <>
+      <Head>
+        <title>Lost...</title>
+      </Head>
+      <div className="h-screen w-full flex justify-center items-center bg-gray-800 flex-col">
+        <h1 className="text-3xl text-white mb-10">Not Found</h1>
+        <button
+          onClick={() => history.push("/")}
+          className="p-4 rounded-xl bg-gray-500 text-white text-3xl"
+        >
+          Go Back
+        </button>
+      </div>
+    </>
   );
 };
 
