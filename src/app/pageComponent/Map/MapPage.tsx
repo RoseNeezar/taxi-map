844cb34d5eb0ce2318d143d5c0f6bb5ff05ea408
Neezar <rforlyffe@gmaicl.com>
2021-07-19 01:28:21 +0800

    refactor action banner

--- src/app/pageComponent/Map/MapPage.tsx
+++ src/app/pageComponent/Map/MapPage.tsx
@@ -13,6 +13,7 @@ import { nearestOfficeLocation } from "../../utils/nearestOffice";
 import { officeLocation } from "../../utils/officeLocation";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import { useGetDrivers } from "./hooks/useGetDrivers";
+import MapActionsBanner from "./component/MapActionsBanner";
 
 interface ILocation {
   lat: number;
@@ -71,40 +72,13 @@ const MapPage: FC = () => {
       <Head>
         <title>Taxi Location</title>
       </Head>
-      <div className=" flex flex-row absolute top-10 left-0 z-20   w-full  justify-center">
-        <div className=" flex flex-row  w-96 bg-gray-500 p-3 rounded-xl">
-          <input
-            type="range"
-            min="1"
-            max="10"
-            defaultValue="1"
-            onChange={(e) => setSlider(Number(e.target.value))}
-            className="w-full h-10 mr-10 transition-all cursor-pointer outline-none opacity-70 hover:opacity-100 rounded-lg overflow-hidden appearance-none bg-gray-400"
-          />
-          <p className="self-center text-white text-xl whitespace-nowrap">
-            {slider} taxi
-          </p>
-        </div>
-
-        <button
-          onClick={() => switchOfficeUK()}
-          className="p-3 mx-10 text-white bg-gray-500 rounded-md"
-        >
-          London office
-        </button>
-        <button
-          onClick={() => switchOfficeSG()}
-          className="mr-10 p-3 text-white bg-gray-500 rounded-md"
-        >
-          SG office
-        </button>
-        <button
-          onClick={() => switcNearestLocation()}
-          className="p-3 text-white bg-gray-500 rounded-md"
-        >
-          nearest Location
-        </button>
-      </div>
+      <MapActionsBanner
+        setSlider={setSlider}
+        slider={slider}
+        switcNearestLocation={switcNearestLocation}
+        switchOfficeSG={switchOfficeSG}
+        switchOfficeUK={switchOfficeUK}
+      />
       <GoogleMap
         id="map"
         mapContainerStyle={mapContainerStyle}
