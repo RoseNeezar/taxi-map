6b5213c52aaf45e3bf9043eab3932a40977fea86
Neezar <rforlyffe@gmaicl.com>
2021-07-18 22:58:53 +0800

    add office locations

---
+++ src/app/utils/officeLocation.ts
@@ -0,0 +1,18 @@
+interface IlocationData {
+  name: string;
+  latitude: number;
+  longitude: number;
+}
+
+export const officeLocation: IlocationData[] = [
+  {
+    name: "SG",
+    latitude: 1.285194,
+    longitude: 103.8522982,
+  },
+  {
+    name: "London",
+    latitude: 51.5049375,
+    longitude: -0.0964509,
+  },
+];
